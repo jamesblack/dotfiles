99df9cbb5d1242a56bf6daeabc717f8ae3da4bc7
James Black <james@james-black.com>
2014-11-05 14:56:44 -0700

    Now installs and sets up SublimeSettings

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo "Installing GitConfig"
+function install {
+	local dotGitConfig=$HOME/.gitconfig
+	local sublimeSettings=$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
-if [ -a $HOME/.gitconfig ]; then
-    echo ".gitconfig exists, creating backup as .gitconfig-backup"
-    mv $HOME/.gitconfig $HOME/.gitconfig-backup
-fi
+	echo "Installing GitConfig"
 
-ln -s $HOME/my/dotfiles/gitconfig $HOME/.gitconfig
+	if [ -a $dotGitConfig ]; then
+	    echo ".gitconfig exists, creating backup as .gitconfig-backup"
+	    mv -f $dotGitConfig $dotGitConfig-backup
+	fi
+
+	ln -s $HOME/my/dotfiles/gitconfig $dotGitConfig
+
+	if [ -a "$sublimeSettings" ]; then
+		echo "sublimesettings exist, backing up as Preferences.sublime-settings.backup"
+		mv -f "$sublimeSettings" "$sublimeSettings".backup
+	fi
+
+	ln -s $HOME/my/dotfiles/Preferences.sublime-settings "$sublimeSettings"
+}
+
+install
 
 echo "DotFile Installation finished! Thanks!"
