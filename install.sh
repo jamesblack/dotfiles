e1fb421a18bbeae7a40fb82568cb5550735d43dc
James Black <james@james-black.com>
2014-11-05 14:35:15 -0700

    Fixes all the things

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@
 
 echo "Installing GitConfig"
 
-echo 
-
 if [ -a $HOME/.gitconfig ]; then
     echo ".gitconfig exists, creating backup as .gitconfig-backup"
     mv $HOME/.gitconfig $HOME/.gitconfig-backup
 fi
-
 
 ln -s $HOME/my/dotfiles/gitconfig $HOME/.gitconfig
 
