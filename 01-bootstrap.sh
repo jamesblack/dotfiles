02746345eef12579ac2cff196bb71748333f5d39
James Black <james@james-black.com>
2016-01-06 12:38:48 -0700

    Move ohmyzsh into its own thing

--- 01-bootstrap.sh
+++ 01-bootstrap.sh
@@ -1,5 +1,3 @@
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 
 source ~/.nvm/nvm.sh
